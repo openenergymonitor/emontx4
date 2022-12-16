4b35a4b7df7900e2d322974d705e6bcbcbcc883d
Glyn Hudson <glynhudson@users.noreply.github.com>
2022-12-16 21:50:51 +0000

    Fix node ID in example emonhub config

--- firmware/EmonTxV4/EmonTxV4.ino
+++ firmware/EmonTxV4/EmonTxV4.ino
@@ -37,7 +37,7 @@ emonhub.conf node decoder (nodeid is 17 when switch is off, 18 when switch is on
 See: https://github.com/openenergymonitor/emonhub/blob/emon-pi/configuration.md
 copy the following into emonhub.conf:
 
-[[15]]
+[[17]]
   nodename = emonTx4cm15
   [[[rx]]]
     names = MSG, Vrms, P1, P2, P3, P4, P5, P6, E1, E2, E3, E4, E5, E6, T1, T2, T3, pulse
