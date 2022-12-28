a94aa6632ca1b8525b88d281c4bebd761f938c50
TrystanLea <trystan.lea@gmail.com>
2022-12-28 11:50:27 +0000

    fix node decoder

--- firmware/EmonTxV4_heatpump/EmonTxV4_heatpump.ino
+++ firmware/EmonTxV4_heatpump/EmonTxV4_heatpump.ino
@@ -39,12 +39,12 @@ See: https://github.com/openenergymonitor/emonhub/blob/emon-pi/configuration.md
 copy the following into emonhub.conf:
 
 [[17]]
-  nodename = emonTx4cm15
+  nodename = emonTx4_17
   [[[rx]]]
-    names = MSG, Vrms, P1, P2, P3, P4, P5, P6, E1, E2, E3, E4, E5, E6, T1, T2, T3, pulse
-    datacodes = L,h,h,h,h,h,h,h,l,l,l,l,l,l,h,h,h,L
-    scales = 1,0.01,1,1,1,1,1,1,1,1,1,1,0.01,0.01,0.01,1
-    units = n,V,W,W,W,W,W,W,Wh,Wh,Wh,Wh,Wh,Wh,C,C,C,p
+    names = MSG, Vrms, P1, P2, P3, E1, E2, E3, analog, flowrate, heat, FT, RT, FT2, RT2, pulse
+    datacodes = L,h,h,h,h,l,l,l,h,h,h,h,h,h,h,L
+    scales = 1,0.01,1,1,1,1,1,1,1,0.01,1,0.01,0.01,0.01,0.01,1
+    units = n,V,W,W,W,Wh,Wh,Wh,a,Lmin,W,C,C,C,C,p
 
 */
 // Comment/Uncomment as applicable
